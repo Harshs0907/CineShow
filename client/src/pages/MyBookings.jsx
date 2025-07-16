2fe20bdac1a3e1a29feb4e8bccfa2732dd99edf1
Harshs0907 <sisodiyaharsh70@gmail.com>
2025-07-16 11:36:27 +0530

    stripe added

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -2,22 +2,37 @@ import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import Loading from '../components/Loading'
+import { useAppContext } from '../context/AppContext'
 import { dateFormat } from '../lib/dateFormat'
 import timeFormat from '../lib/timeFormat'
 
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY
 
+  const {axios, getToken, user, image_base_url} = useAppContext()
+
   const [bookings, setBookings] = useState([])
   const [isLoading, setIsLoading] = useState(true)
    const getMyBookings = async () =>{
-    setBookings(dummyBookingData)
+    try {
+      const {data} = await axios.get('/api/user/bookings',{
+        headers: { Authorization: `Bearer ${await getToken()}`}
+      })
+
+      if(data.success){
+        setBookings(data.bookings)
+      }
+    } catch (error) {
+      console.log(error)
+    }
     setIsLoading(false)
    }
 
    useEffect(()=>{
-    getMyBookings()
-   },[])
+    if(user){
+      getMyBookings()
+    }
+   },[user])
 
   return !isLoading ? (
     <div className=' relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
@@ -29,7 +44,7 @@ const MyBookings = () => {
       {bookings.map((item, index)=>(
         <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mmt-4 p-2 max-w-3xl'>
           <div>
-            <img src={item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'/>
+            <img src={image_base_url + item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'/>
             <div className='flex flex-col p-4'>
               <p className='text-lg font-semibold'>{item.show.movie.title}</p>
               <p className='text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
